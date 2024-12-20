5fa3ee2fdcff111ed7ca0b56aebf4586bff76ced
Sathesh Previn <prevsathesh3215@gmail.com>
2024-12-21 00:31:21 +0800

    add validation function

--- src/app/my-form/my-form.component.ts
+++ src/app/my-form/my-form.component.ts
@@ -1,19 +1,44 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { NgForm } from '@angular/forms'
+import { ReactiveFormsModule,  FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-my-form',
-  imports: [FormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './my-form.component.html',
   standalone: true,
   styleUrl: './my-form.component.css'
 })
 export class MyFormComponent {
 
-  submit(login: NgForm){
-    console.log('Form submitted.', login)
+  registrationForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.registrationForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      fullName: ['', [Validators.required, Validators.minLength(3)]],
+      mobileNumber: ['', [Validators.required, Validators.pattern(/^\d{10,12}$/)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required]],
+      terms: [false, [Validators.requiredTrue]],
+    }, {
+      validators: this.passwordMatchValidator,
+    });
   }
+
+  passwordMatchValidator(group: FormGroup) {
+    const password = group.get('password')?.value;
+    const confirmPassword = group.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { mismatch: true };
+  }
+
+  onSubmit() {
+    if (this.registrationForm.valid) {
+      console.log('Form Data:', this.registrationForm.value);
+    } else {
+      console.log('Form is invalid!');
+    }
+  }
+
 
 }
